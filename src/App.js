001b2855548c94a70feb96db837ee95b6eacfaa4
Daniayr <abakirov851927@gmail.com>
2021-04-16 16:56:27 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  export default function App() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/topics">
+              <Topics />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+
+  function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    );
+  }
+
+  function Topics() {
+    let { path, url } = useRouteMatch();
+
+    return (
+      <div>
+        <h2>Topics</h2>
+        <ul>
+          <li>
+            <Link to={`${url}/rendering`}>Rendering with React</Link>
+          </li>
+          <li>
+            <Link to={`${url}/components`}>Components</Link>
+          </li>
+          <li>
+            <Link to={`${url}/props-v-state`}>Props v. State</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path={path}>
+            <h3>Please select a topic.</h3>
+          </Route>
+          <Route path={`${path}/:topicId`}>
+            <Topic />
+          </Route>
+        </Switch>
+      </div>
+    );
 }
 
-export default App;
+  function Topic() {
+  let { topicId } = useParams();
+
+    return (
+      <div>
+        <h3>{topicId}</h3>
+      </div>
+    );
+}
